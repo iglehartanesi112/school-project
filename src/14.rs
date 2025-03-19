045dea4c5fba532ae70e49dd00d162e1b614ac84
iglehartanesi112 <iglehartanesi112@gmail.com>
2025-03-19 04:17:14 +0000

    Add 14.rs

---
+++ src/14.rs
@@ -0,0 +1,7 @@
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let num1: u32 = rng.gen_range(1..=10);
+    let num2: u32 = rng.gen_range(1..=10);
+    println!("{} + {} =", num1, num2);
+}
